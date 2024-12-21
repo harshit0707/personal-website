9b467457d9516913dd258983f4cd40f0af0dda3b
Harshit <harshit070797@gmail.com>
2024-12-21 15:03:07 +0100

    update nuxt config for ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  routeRules: {
-    '/': { prerender: true }
-  }
-})
+    ssr: true,
+    devtools: { enabled: true },
+    modules: ['@nuxt/content'],
+    routeRules: {
+        '/': { prerender: true },
+    },
+});
